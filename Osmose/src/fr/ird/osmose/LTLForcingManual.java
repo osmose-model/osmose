1cc47aefc7d446eb902c12c47bd3b2aad74b95c1
morgane <morgane@45b28200-7c74-4d1a-96bc-342942c5875a>
2012-04-30 20:46:54 +0000

    LTLForcingManual.java java class for reading LTL input file written in csv and already vertically integrated and defined already on the osmose grid
    git-svn-id: https://subversion.renater.fr/authscm/barrier.n/svn/osmose/trunk@167 45b28200-7c74-4d1a-96bc-342942c5875a

---
+++ Osmose/src/fr/ird/osmose/LTLForcingManual.java
@@ -0,0 +1,160 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.ird.osmose;
+
+
+import au.com.bytecode.opencsv.CSVReader;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StreamTokenizer;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import ucar.ma2.Array;
+import ucar.ma2.ArrayDouble;
+import ucar.ma2.ArrayFloat;
+import ucar.ma2.Index;
+import ucar.nc2.Attribute;
+import ucar.nc2.NetcdfFile;
+import ucar.nc2.Variable;
+
+/**
+ *
+ * @author mtravers
+ */
+public class LTLForcingManual extends AbstractLTLForcing {
+
+	
+	private String[][] planktonFileList;
+	
+	
+	@Override
+	public void readLTLConfigFile2(String planktonFileName) {
+        FileInputStream LTLFile;
+        try {
+            LTLFile = new FileInputStream(new File(getOsmose().inputPathName, planktonFileName));
+        } catch (FileNotFoundException ex) {
+            System.out.println("LTL file " + planktonFileName + " doesn't exist");
+            return;
+        }
+
+        Reader r = new BufferedReader(new InputStreamReader(LTLFile));
+        StreamTokenizer st = new StreamTokenizer(r);
+        st.slashSlashComments(true);
+        st.slashStarComments(true);
+        st.quoteChar(';');
+
+        try {
+        	planktonFileList = new String[getNbPlanktonGroups()][];
+        	for (int gr = 0; gr< getNbPlanktonGroups(); gr++){
+        		planktonFileList[gr] = new String[getNbForcingDt()];
+        		for (int step = 0; step < getNbForcingDt(); step++) {
+        			st.nextToken();
+        			planktonFileList[gr][step] = st.sval;
+        		}
+            }
+
+        } catch (IOException ex) {
+            System.out.println("Reading error of LTL file");
+            return;
+        }	
+	}
+
+	@Override
+	public void initPlanktonMap() {
+        /*
+         * Associate osmose cells to BFM cells
+         */
+        for (int i = 0; i < getGrid().getNbLines(); i++) {
+            for (int j = 0; j < getGrid().getNbColumns(); j++) {
+                getGrid().getCell(i, j).icoordLTLGrid.addElement(i);
+                getGrid().getCell(i, j).jcoordLTLGrid.addElement(j);
+            }
+        }
+		/*
+         * Initialize plankton list
+         */
+        initPlanktonList();		
+	}
+
+	@Override
+    public void updatePlankton(int dt) {
+        for (int i = 0; i < getNbPlanktonGroups(); i++) {
+            getPlanktonGroup(i).clearPlankton();      // put the biomass tables of plankton to 0
+        }
+        for (int p = 0; p < getNbPlanktonGroups(); p++) {
+            String nameTemp = getOsmose().resolveFile(planktonFileList[p][dt]);
+            readCSVFile(nameTemp,p);
+        }
+       
+        mapInterpolation();
+        
+        if (getSimulation().getYear() >= getOsmose().timeSeriesStart) {
+            saveForDiet();       // save biomass of plankton before predation
+        }
+    }
+
+
+	@Override
+	public void mapInterpolation() {
+	      int tempX, tempY;
+	        for (int i = 0; i < getGrid().getNbLines(); i++) {
+	            for (int j = 0; j < getGrid().getNbColumns(); j++) {
+	                if (!getGrid().getCell(i, j).isLand()) {
+	                    for (int k = 0; k < getGrid().getCell(i, j).getNbCellsLTLGrid(); k++) {
+	                        for (int p = 0; p < getNbPlanktonGroups(); p++) {
+	                            tempY = ((Integer) getGrid().getCell(i, j).icoordLTLGrid.elementAt(k)).intValue();
+	                            tempX = ((Integer) getGrid().getCell(i, j).jcoordLTLGrid.elementAt(k)).intValue();
+	                            /*if (p == 0) {
+	                            System.out.println("osmose cell (" + i + ", " + j + ") contains ECO3M cell (" + tempX + ", " + tempY + ")");
+	                            }*/
+	                            // interpolate the plankton concentrations from the LTL cells
+	                            getPlanktonGroup(p).addCell(i, j, tempX, tempY, getGrid().getCell(i, j).getNbCellsLTLGrid());
+	                        }
+	                    }
+	                }
+	            }
+	        }
+	}
+	
+	
+	private void readCSVFile(String nameOfFile,int planktonGroup) {
+
+        //System.out.println("Reading " + nameOfFile);
+        String name = nameOfFile;
+        try {
+            /*
+             * Read the CSV file
+             */
+            CSVReader reader = new CSVReader(new FileReader(name), ';');
+            List<String[]> lines = reader.readAll();
+         
+            if(!(lines.size()==getPlanktonDimY())){
+            	System.out.println("No match between nb line of csv and dimY");
+            }
+            for (int i = 0; i < lines.size(); i++) {
+                String[] line = lines.get(i);
+                if(!(line.length==getPlanktonDimX())){
+                	System.out.println("No match between nb column of csv and dimX");
+                }
+                
+                for (int j = 0; j < line.length; j++) {
+     //          	getPlanktonGroup(planktonGroup).dataInit[j][i][0] = Float.valueOf(line[j]);                 
+                    getPlanktonGroup(planktonGroup).integratedData[j][i] = Float.valueOf(line[j]);
+                }
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Osmose.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        //System.out.println("Read CSV file " + csvFile + " [OK]");
+    }
+
+}
