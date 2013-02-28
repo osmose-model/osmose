1af97a179cb63caf55cadd90e7074fb3f47093c0
pverley <pverley@45b28200-7c74-4d1a-96bc-342942c5875a>
2013-02-28 17:30:05 +0000

    Added new class ConfigurationConverter.java that will help to convert from old format of configuration files to new format.
    git-svn-id: https://subversion.renater.fr/authscm/barrier.n/svn/osmose/trunk@543 45b28200-7c74-4d1a-96bc-342942c5875a

---
+++ src/fr/ird/osmose/util/ConfigurationConverter.java
@@ -0,0 +1,55 @@
+package fr.ird.osmose.util;
+
+import fr.ird.osmose.Configuration;
+import fr.ird.osmose.Osmose;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author pverley
+ */
+public class ConfigurationConverter {
+
+    private Configuration cfg;
+    private Properties prop;
+    
+    ConfigurationConverter(String[] args) {
+        
+        // Get old configuration
+        Osmose osmose = Osmose.getInstance();
+        osmose.init(args);
+        cfg = osmose.getConfiguration();
+        
+        // Create new Properties
+        prop = new Properties();
+        
+        // convert
+        convert();
+        write();
+    }
+    
+    private void convert() {
+        prop.setProperty("key", "value");
+    }
+    
+    private void write() {
+        try {
+            prop.store(new FileWriter(cfg.resolveFile(getFilename())), null);
+        } catch (IOException ex) {
+            Logger.getLogger(ConfigurationConverter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    String getFilename() {
+        StringBuilder filename = new StringBuilder(cfg.getOutputPrefix());
+        filename.append("_all-parameters.cfg");
+        return filename.toString();
+    }
+
+    public static void main(String[] args) {
+        new ConfigurationConverter(args).convert();
+    }
+}
