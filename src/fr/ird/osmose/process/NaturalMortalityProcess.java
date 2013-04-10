9f4ac200e07fd152148a4ff79fbf8f1e2c2c1ff4
pverley <pverley@45b28200-7c74-4d1a-96bc-342942c5875a>
2013-04-10 08:56:59 +0000

    (NaturalMortalityProcess.java) It now takes spatial mortality rate. It looks for parameter "mortality.natural.rate.file.sp#"
    git-svn-id: https://subversion.renater.fr/authscm/barrier.n/svn/osmose/trunk@619 45b28200-7c74-4d1a-96bc-342942c5875a

--- src/fr/ird/osmose/process/NaturalMortalityProcess.java
+++ src/fr/ird/osmose/process/NaturalMortalityProcess.java
@@ -1,7 +1,15 @@
 package fr.ird.osmose.process;
 
+import au.com.bytecode.opencsv.CSVReader;
+import fr.ird.osmose.Osmose;
 import fr.ird.osmose.School;
 import fr.ird.osmose.Species;
+import fr.ird.osmose.util.GridMap;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -13,6 +21,10 @@ public class NaturalMortalityProcess extends AbstractProcess {
      * Natural mortality rates year-1.
      */
     private float D[];
+    /**
+     *
+     */
+    private GridMap[] spatialD;
     /**
      * Larval mortality rates, timestep-1.
      */
@@ -36,6 +48,16 @@ public class NaturalMortalityProcess extends AbstractProcess {
         for (int iSpec = 0; iSpec < getConfiguration().getNSpecies(); iSpec++) {
             for (int iStep = 0; iStep < larvalMortalityRates[iSpec].length; iStep++) {
                 larvalMortalityRates[iSpec][iStep] = getConfiguration().getFloat("mortality.natural.larva.rate.sp" + iSpec);
+            }
+        }
+
+        spatialD = new GridMap[getNSpecies()];
+        List<String> keys = getConfiguration().findKeys("mortality.natural.rate.file.sp*");
+        if (keys != null && !keys.isEmpty()) {
+            for (int iSpec = 0; iSpec < getConfiguration().getNSpecies(); iSpec++) {
+                if (getConfiguration().canFind("mortality.natural.rate.file.sp" + iSpec)) {
+                    spatialD[iSpec] = readCSVMap(getConfiguration().getFile("mortality.natural.rate.file.sp" + iSpec));
+                }
             }
         }
 
@@ -70,7 +92,11 @@ public class NaturalMortalityProcess extends AbstractProcess {
         if (school.getAgeDt() == 0) {
             M = (larvalMortalityRates[spec.getIndex()][getSimulation().getIndexTimeSimu()] + migration.getOutMortality(school)) / (float) subdt;
         } else {
-            M = (D[spec.getIndex()] + migration.getOutMortality(school)) / (float) (getConfiguration().getNStepYear() * subdt);
+            if (null != spatialD[spec.getIndex()] && !school.isUnlocated()) {
+                M = (spatialD[spec.getIndex()].getValue(school.getCell()) + migration.getOutMortality(school)) / (float) (getConfiguration().getNStepYear() * subdt);
+            } else {
+                M = (D[spec.getIndex()] + migration.getOutMortality(school)) / (float) (getConfiguration().getNStepYear() * subdt);
+            }
         }
         return M;
     }
@@ -92,5 +118,39 @@ public class NaturalMortalityProcess extends AbstractProcess {
 
     public double getNaturalMortalityRate(Species species) {
         return D[species.getIndex()];
+    }
+
+    private GridMap readCSVMap(String csvFile) {
+
+        GridMap map = null;
+        try {
+            /*
+             * Read the CSV file
+             */
+            CSVReader reader = new CSVReader(new FileReader(csvFile), ';');
+            List<String[]> lines = reader.readAll();
+            /*
+             * Initialize the map
+             */
+            map = new GridMap();
+            /*
+             * Read the map
+             */
+            int ny = getGrid().get_ny();
+            for (int l = 0; l < lines.size(); l++) {
+                String[] line = lines.get(l);
+                int j = ny - l - 1;
+                for (int i = 0; i < line.length; i++) {
+                    float val = Float.valueOf(line[i]);
+                    if (val > 0.f) {
+                        map.setValue(i, j, val);
+                    }
+                }
+            }
+            reader.close();
+        } catch (IOException ex) {
+            getLogger().log(Level.SEVERE, "Error reading natural mortality rate file " + csvFile, ex);
+        }
+        return map;
     }
 }
