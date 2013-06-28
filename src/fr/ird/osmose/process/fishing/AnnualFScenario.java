b8899d9b10b5cbd30ba970c05e5f1734cc7b18e4
pverley <pverley@45b28200-7c74-4d1a-96bc-342942c5875a>
2013-06-28 16:51:39 +0000

    Created new package fishing to handle all the different parametrization of fishing scenarii. Added class AbstractFishingScenario.java that provided a template for any fishing scenario. Created two instances, for annual F and annual F * seasonality
    These new classes are not plugged in the model yet.
    
    git-svn-id: https://subversion.renater.fr/authscm/barrier.n/svn/osmose/trunk@723 45b28200-7c74-4d1a-96bc-342942c5875a

---
+++ src/fr/ird/osmose/process/fishing/AnnualFScenario.java
@@ -0,0 +1,50 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.ird.osmose.process.fishing;
+
+import fr.ird.osmose.School;
+import fr.ird.osmose.Species;
+import java.util.logging.Level;
+
+/**
+ *
+ * @author pverley
+ */
+public class AnnualFScenario extends AbstractFishingScenario {
+
+    private float instantaneousF;
+    private int recruitmentAge;
+    private float recruitmentSize;
+
+    public AnnualFScenario(int iSimulation, Species species) {
+        super(iSimulation, species);
+    }
+
+    @Override
+    public void init() {
+        int nStepYear = getConfiguration().getNStepYear();
+        int iSpec = getIndexSpecies();
+        instantaneousF = getConfiguration().getFloat("mortality.fishing.rate.sp" + iSpec) / nStepYear;
+        if (!getConfiguration().isNull("mortality.fishing.recruitment.age.sp" + iSpec)) {
+            float age = getConfiguration().getFloat("mortality.fishing.recruitment.age.sp" + iSpec);
+            recruitmentAge = Math.round(age * nStepYear);
+            recruitmentSize = 0.f;
+        } else if (!getConfiguration().isNull("mortality.fishing.recruitment.size.sp" + iSpec)) {
+            recruitmentSize = getConfiguration().getFloat("mortality.fishing.recruitment.size.sp" + iSpec);
+            recruitmentAge = 0;
+        } else {
+            recruitmentAge = 0;
+            recruitmentSize = 0.f;
+            getLogger().log(Level.WARNING, "Could not find any fishing recruitment threshold (neither age nor size) for species {0}. Osmose assumes every school can be catched.", getSpecies().getName());
+        }
+    }
+
+    @Override
+    public float getFishingMortalityRate(School school) {
+        return (school.getAgeDt() >= recruitmentAge) && (school.getLength() >= recruitmentSize)
+                ? instantaneousF
+                : 0.f;
+    }
+}
---
+++ src/fr/ird/osmose/process/fishing/AnnualFSeasonScenario.java
@@ -0,0 +1,74 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.ird.osmose.process.fishing;
+
+import au.com.bytecode.opencsv.CSVReader;
+import fr.ird.osmose.School;
+import fr.ird.osmose.Species;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+
+/**
+ *
+ * @author pverley
+ */
+public class AnnualFSeasonScenario extends AbstractFishingScenario {
+
+    private float annualF;
+    private float[] season;
+    private int recruitmentAge;
+    private float recruitmentSize;
+
+    public AnnualFSeasonScenario(int iSimulation, Species species) {
+        super(iSimulation, species);
+    }
+
+    @Override
+    public void init() {
+        int nStepYear = getConfiguration().getNStepYear();
+        int iSpec = getIndexSpecies();
+        annualF = getConfiguration().getFloat("mortality.fishing.rate.sp" + iSpec);
+        if (!getConfiguration().isNull("mortality.fishing.recruitment.age.sp" + iSpec)) {
+            float age = getConfiguration().getFloat("mortality.fishing.recruitment.age.sp" + iSpec);
+            recruitmentAge = Math.round(age * nStepYear);
+            recruitmentSize = 0.f;
+        } else if (!getConfiguration().isNull("mortality.fishing.recruitment.size.sp" + iSpec)) {
+            recruitmentSize = getConfiguration().getFloat("mortality.fishing.recruitment.size.sp" + iSpec);
+            recruitmentAge = 0;
+        } else {
+            recruitmentAge = 0;
+            recruitmentSize = 0.f;
+            getLogger().log(Level.WARNING, "Could not find any fishing recruitment threshold (neither age nor size) for species {0}. Osmose assumes every school can be catched.", getSpecies().getName());
+        }
+
+
+        season = new float[nStepYear];
+        String filename = getConfiguration().getFile("mortality.fishing.season.distrib.file.sp" + iSpec);
+        CSVReader reader;
+        try {
+            reader = new CSVReader(new FileReader(filename), ';');
+            List<String[]> lines = reader.readAll();
+            if ((lines.size() - 1) != nStepYear) {
+                throw new IOException("Wrong number of time steps in file. Found " + (lines.size() - 1) + " and expected " + nStepYear);
+            }
+            for (int t = 0; t < nStepYear; t++) {
+                season[t] = Float.valueOf(lines.get(t + 1)[1]);
+            }
+        } catch (IOException ex) {
+            getLogger().log(Level.SEVERE, "Error reading fishing seasonality file " + filename, ex);
+        }
+
+
+    }
+
+    @Override
+    public float getFishingMortalityRate(School school) {
+        return (school.getAgeDt() >= recruitmentAge) && (school.getLength() >= recruitmentSize)
+                ? annualF * season[getSimulation().getIndexTimeYear()]
+                : 0.f;
+    }
+}
