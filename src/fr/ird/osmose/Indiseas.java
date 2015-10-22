1c6d7e3ee82dae97850e2b42f3d417b4c1688fc0
pverley <pverley@45b28200-7c74-4d1a-96bc-342942c5875a>
2015-10-22 12:08:10 +0000

    (Indiseas.java) Added the responsiveness() function. Fixed -D options (JVM) by -P options for passing command line parameters Sort overridden parameters in the summary text file.
    git-svn-id: https://subversion.renater.fr/authscm/barrier.n/svn/osmose/trunk@1338 45b28200-7c74-4d1a-96bc-342942c5875a

--- src/fr/ird/osmose/Indiseas.java
+++ src/fr/ird/osmose/Indiseas.java
@@ -5,6 +5,7 @@
  */
 package fr.ird.osmose;
 
+import au.com.bytecode.opencsv.CSVWriter;
 import fr.ird.osmose.util.logging.OLogger;
 import java.io.File;
 import java.io.FileWriter;
@@ -12,6 +13,8 @@ import java.io.IOException;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
 import org.apache.commons.math3.distribution.LogNormalDistribution;
 
 /**
@@ -20,31 +23,23 @@ import org.apache.commons.math3.distribution.LogNormalDistribution;
  */
 public class Indiseas extends OLogger {
 
+    // Name of the scenarii
     private final String[] scenarii = new String[]{"ltl", "htl", "all"};
+    // Index of the species for every scenario
     private int[][] species;
     // Fmsy, maximum sustainable yield
     private float[] fmsy;
-    // Sensitivity Fmsy multiplier
-    private float[] svFx;
-    // Specificity Fmsy multiplier
-    private float[] spFx;
-    // Specificity directional plankton multiplier
-    private float[] spPx;
-    // Specificity random
-    private double phytoBiomass;
-    private double[] sd;
-    private int ndraw;
-    // Responsiveness
-    private float[] rvFx;
-    private int nyearBurnIn;
-    private int nyearChange;
-    // General indiseas parameters
+    // Osmose main configuration file
     private String osmoseMainCfg;
-    private String outputPath;
+    // Indiseas working directory
+    private String wdPath;
 
+    /**
+     * 
+     */
     public void init() {
 
-        osmoseMainCfg = getConfiguration().getString("osmose.configuration.main");
+        osmoseMainCfg = getConfiguration().getFile("osmose.configuration.main");
 
         species = new int[scenarii.length][];
         for (int is = 0; is < scenarii.length; is++) {
@@ -57,7 +52,7 @@ public class Indiseas extends OLogger {
             fmsy[ispec] = getConfiguration().getFloat("indiseas.fmsy.sp" + ispec);
         }
 
-        outputPath = getConfiguration().getFile("indiseas.output.path");
+        wdPath = getConfiguration().getFile("indiseas.output.path");
 
         try (FileWriter fw = new FileWriter(getConfiguration().getFile("indiseas.simulation.file"), false)) {
             fw.write("# ");
@@ -68,21 +63,33 @@ public class Indiseas extends OLogger {
         }
     }
 
+    /**
+     * Create simulation launchers for three types of experience: sensitivity,
+     * specificity and responsiveness of the indicators.
+     */
     public void run() {
-        //sensitivity();
+        sensitivity();
         specificity();
+        responsiveness();
     }
 
+    /**
+     * Sensitivity: does the indicator vary significantly under fishing
+     * pressure.
+     */
     private void sensitivity() {
 
-        svFx = getConfiguration().getArrayFloat("indiseas.sensitivy.fmsy.multiplier");
+        // Sensitivity Fmsy multiplier
+        float[] svFx = getConfiguration().getArrayFloat("indiseas.sensitivy.fmsy.multiplier");
 
-        for (int is = 0; is < scenarii.length; is++) {
+        for (int is = 0;
+                is < scenarii.length;
+                is++) {
             for (int iF = 0; iF < svFx.length; iF++) {
                 HashMap<String, String> options = new HashMap();
                 // Output folder
                 StringBuilder destination = new StringBuilder();
-                destination.append(outputPath);
+                destination.append(wdPath);
                 destination.append(File.separator);
                 destination.append("sv_");
                 destination.append(scenarii[is]);
@@ -106,11 +113,20 @@ public class Indiseas extends OLogger {
         }
     }
 
+    /**
+     * Specificity: does the indicator respond primarily to fishing compared to
+     * other drivers. Two types of environmental variability: random
+     * environmental variation, and directional environmental forcing. These two
+     * types of forcing intend to represent interannual variability and longer
+     * term, directional climate change.
+     */
     private void specificity() {
 
-        // Directional
-        spFx = getConfiguration().getArrayFloat("indiseas.specificity.fmsy.multiplier");
-        spPx = getConfiguration().getArrayFloat("indiseas.specificity.plankton.multiplier");
+        //// Directional
+        // Specificity Fmsy multiplier
+        float[] spFx = getConfiguration().getArrayFloat("indiseas.specificity.fmsy.multiplier");
+        // Specificity directional plankton multiplier
+        float[] spPx = getConfiguration().getArrayFloat("indiseas.specificity.plankton.multiplier");
 
         for (int is = 0; is < scenarii.length; is++) {
             for (int iF = 0; iF < spFx.length; iF++) {
@@ -118,7 +134,7 @@ public class Indiseas extends OLogger {
                     HashMap<String, String> options = new HashMap();
                     // Output folder
                     StringBuilder destination = new StringBuilder();
-                    destination.append(outputPath);
+                    destination.append(wdPath);
                     destination.append(File.separator);
                     destination.append("sp_");
                     destination.append(scenarii[is]);
@@ -148,10 +164,13 @@ public class Indiseas extends OLogger {
             }
         }
 
-        // Random
+        //// Random
+        // Phytoplankton biomass
         double bm = getConfiguration().getDouble("indiseas.specificity.phytoplankton.biomass");
-        sd = getConfiguration().getArrayDouble("indiseas.specificity.random.sd");
-        ndraw = getConfiguration().getInt("indiseas.specificity.random.ndraw");
+        // Standard deviation
+        double[] sd = getConfiguration().getArrayDouble("indiseas.specificity.random.sd");
+        // Number of draws for a given standard deviation
+        int ndraw = getConfiguration().getInt("indiseas.specificity.random.ndraw");
         for (int is = 0; is < scenarii.length; is++) {
             for (int iF = 0; iF < spFx.length; iF++) {
                 for (int isd = 0; isd < sd.length; isd++) {
@@ -162,7 +181,7 @@ public class Indiseas extends OLogger {
                         HashMap<String, String> options = new HashMap();
                         // Output folder
                         StringBuilder destination = new StringBuilder();
-                        destination.append(outputPath);
+                        destination.append(wdPath);
                         destination.append(File.separator);
                         destination.append("sp_");
                         destination.append(scenarii[is]);
@@ -198,6 +217,70 @@ public class Indiseas extends OLogger {
         }
     }
 
+    /**
+     * Responsiveness: does the indicator respond rapidly to fishing.
+     */
+    private void responsiveness() {
+
+        // Fmsy multiplier
+        float[] rvFx = getConfiguration().getArrayFloat("indiseas.responsiveness.fmsy.multiplier");
+        // Number of years of spinup with F = Fcurrent
+        int nyearBurnIn = getConfiguration().getInt("indiseas.responsiveness.burnin.nyear");
+        // Number of years with F = Fmsy * multiplier
+        int nyearChange = getConfiguration().getInt("indiseas.responsiveness.change.nyear");
+
+        // Simulation duration
+        int nyearTot = nyearBurnIn + nyearChange;
+        // Monthly output
+        int dtOut = getConfiguration().getInt("simulation.time.ndtPerYear") / 12;
+
+        for (int is = 0; is < scenarii.length; is++) {
+            for (int iF = 0; iF < rvFx.length; iF++) {
+                HashMap<String, String> options = new HashMap();
+                // Output folder
+                StringBuilder destination = new StringBuilder();
+                destination.append(wdPath);
+                destination.append(File.separator);
+                destination.append("rv_");
+                destination.append(scenarii[is]);
+                destination.append("_fx");
+                destination.append(rvFx[iF]);
+                options.put("output.dir.path", destination.toString());
+                new File(destination.toString()).mkdirs();
+                for (int ispec = 0; ispec < species[is].length; ispec++) {
+                    int ispecies = species[is][ispec];
+                    float F1 = getConfiguration().getFloat("mortality.fishing.rate.sp" + ispec);
+                    float F2 = rvFx[iF] * fmsy[ispecies];
+                    StringBuilder csvfile = new StringBuilder();
+                    csvfile.append(destination);
+                    csvfile.append(File.separator);
+                    csvfile.append("fishing-rate-by-year-sp");
+                    csvfile.append(ispec);
+                    csvfile.append(".csv");
+                    try (CSVWriter writer = new CSVWriter(new FileWriter(csvfile.toString()), ';')) {
+                        for (int iy = 0; iy < nyearTot; iy++) {
+                            float F = (iy < nyearBurnIn) ? F1 : F2;
+                            String[] newline = new String[]{String.valueOf(iy), String.valueOf(F)};
+                            writer.writeNext(newline);
+                        }
+                        info("Created fishing mortality file by year for species " + ispecies + " " + csvfile);
+                    } catch (IOException ex) {
+                        error("Error writing CSV file " + csvfile, ex);
+                    }
+                    options.put("mortality.fishing.rate.byYear.file.sp" + ispecies, csvfile.toString());
+                }
+                // Monthly output
+                options.put("output.recordfrequency.ndt", String.valueOf(dtOut));
+                // Simulation duration
+                options.put("simulation.time.nyear", String.valueOf(nyearTot));
+                // General options
+                addGeneralOptions(options);
+                // Create java command
+                appendSimulation(options, destination.toString());
+            }
+        }
+    }
+
     private void addGeneralOptions(HashMap<String, String> options) {
 
         // Number of replicated simulations
@@ -211,7 +294,7 @@ public class Indiseas extends OLogger {
         // Append the new simulation command in the indiseas simulation file
         try (FileWriter fw = new FileWriter(getConfiguration().getFile("indiseas.simulation.file"), true)) {
             fw.write(cmd);
-            fw.write('\n');
+            fw.write("\n\n");
         } catch (IOException ex) {
             error("Failed to append command", ex);
         }
@@ -235,10 +318,11 @@ public class Indiseas extends OLogger {
     private String optionsToString(HashMap<String, String> options) {
         StringBuilder txt = new StringBuilder();
         txt.append("Overriden parameters:\n");
-        for (Map.Entry<String, String> argument : options.entrySet()) {
-            txt.append(argument.getKey());
+        SortedSet<String> keys = new TreeSet<>(options.keySet());
+        for (String key : keys) {
+            txt.append(key);
             txt.append(" = ");
-            txt.append(argument.getValue());
+            txt.append(options.get(key));
             txt.append('\n');
         }
         return txt.toString();
@@ -256,7 +340,7 @@ public class Indiseas extends OLogger {
         cmd.append(" ");
         // Osmose parameters 
         for (Map.Entry<String, String> argument : options.entrySet()) {
-            cmd.append("-D");
+            cmd.append("-P");
             cmd.append(argument.getKey());
             cmd.append("=");
             cmd.append(argument.getValue());
@@ -265,7 +349,7 @@ public class Indiseas extends OLogger {
         // main Osmose configuration file
         cmd.append(osmoseMainCfg);
         // Send the log to the output folder
-        cmd.append(" > ");
+        cmd.append(" &> ");
         cmd.append(options.get("output.dir.path"));
         cmd.append(File.separator);
         cmd.append("osmose.log");
