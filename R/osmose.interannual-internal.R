1db003cb025be2fb3db3c4a102512aed632dddf9
barriern <nicolas.barrier@ird.fr>
2017-10-17 12:27:55 +0200

    Adding comments on the .writeFishingFile function.
    Correction of a bug? Fs was written even if not defined.

--- R/osmose.interannual-internal.R
+++ R/osmose.interannual-internal.R
@@ -2,23 +2,32 @@
 
 # Fishing -----------------------------------------------------------------
 
+# Writes fishing files for individual species.
+#
+# @param sp Species number (sp0, sp1, etc)
+# @param fishing Fishing parameters (method, etc.) 
+# @param species Species parameters
+# @param T Number of years
+# @param ndt  Time step
+# @param output Output path
 .writeFishingFile = function(sp, fishing, species, T, ndt, output) {
   
   if(is.null(fishing$rate$method[[sp]])) return(NULL)
   
   fileCode = file.path(output, "fishing_%s_%s.csv")
   
-  f           = getFishingMortality(sp, fishing, T, ndt) 
+  f = getFishingMortality(sp, fishing, T, ndt) 
   write.osmose(f, file=sprintf(fileCode, "byDt", species$name[sp]))
   
   selectivity = getSelectivity(sp, fishing, T, ndt)
   if(!is.null(selectivity)) {
-    Fs          = f %o% selectivity
-    write.osmose(f, file=sprintf(fileCode, "byDt", species$name[sp]))
+    Fs = f %o% selectivity   # outer product
+    #write.osmose(f, file=sprintf(fileCode, "byDt", species$name[sp]))
+    write.osmose(Fs, file=file.path(output, paste0("F-", isp, ".csv")))
     
   }
   
-  fishing = cbind(fishing, f) 
-  write.osmose(Fs, file=file.path(output, paste0("F-", isp, ".csv")))
+  # fishing = cbind(fishing, f)    ????
+  # write.osmose(Fs, file=file.path(output, paste0("F-", isp, ".csv")))
   
 }
