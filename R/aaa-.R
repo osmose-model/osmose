8eb3a0b921d9191f91fbe2d356ee2ff531f94ae9
roliveros-ramos <ricardo.oliveros@gmail.com>
2017-10-18 18:56:36 +0200

    imports!

--- R/aaa-.R
+++ R/aaa-.R
@@ -3,7 +3,11 @@
 NULL
 #' @importFrom grDevices col2rgb dev.off png rgb
 NULL
-#' @importFrom stats median quantile setNames
+#' @importFrom stats median quantile setNames dlnorm dnorm qnorm
 NULL
 #' @importFrom utils choose.dir read.csv read.table write.table
+NULL
+#' @importFrom rlist list.merge
+NULL
+#' @importFrom stringr str_split str_trim str_sub
 NULL
