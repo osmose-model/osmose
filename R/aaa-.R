fd83eaa814f0b42081791bbaa540cdb0c1e76070
barriern <nicolas.barrier@ird.fr>
2018-02-19 12:26:06 +0100

    Adding imports for ggplot2 and NetCDF

--- R/aaa-.R
+++ R/aaa-.R
@@ -11,3 +11,11 @@ NULL
 NULL
 #' @importFrom stringr str_split str_trim str_sub
 NULL
+
+# barrier.n: adding some functions for NETCDF
+#' @importFrom RNetCDF file.inq.nc var.inq.nc var.get.nc open.nc
+NULL
+
+# barrier.n: adding some functions for ggplot
+#' @importFrom ggplot2 ggplot geom_area scale_fill_brewer scale_fill_manual theme ggtitle
+NULL
