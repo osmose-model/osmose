678f4a9905dd2f99d770c4a901bc2ab0521bbc90
barriern <nicolas.barrier@ird.fr>
2018-03-01 08:23:59 +0100

    Change in the function names: no more public getVar but private functions. Called in plotting and summary functions

--- R/osmose-getVar.R
+++ R/osmose-getVar.R
@@ -1,24 +1,14 @@
-#' @param ... Additional arguments of the function.
-#' @return An array or a list containing the data.
-#' @export
-#' @method getVar osmose.output.dietMatrix
-getVar.osmose.output.dietMatrix = function(out, species=NULL, time.mean=FALSE, repl.mean=TRUE, thres=1, ...) {
+# @param ... Additional arguments of the function.
+# @return An array or a list containing the data.
+process.dietMatrix = function(out, species=NULL, time.mean=FALSE, thres=1, ...) {
   
   .check_species(out, species)
-  
-  if(!repl.mean) {
-    warning("The repl.mean argument has been set to True")
-    repl.mean = TRUE
-  }
   
   # extract the given specie
   out = out[[species]]
   
   # Computes the mean over the replicates
-  if(repl.mean)
-  {
-    out = apply(out, c(1, 2), mean)
-  }
+  out = apply(out, c(1, 2), mean)
   
   # computes the time average
   data.time.mean = apply(out, 2, mean)
@@ -40,9 +30,6 @@ getVar.osmose.output.dietMatrix = function(out, species=NULL, time.mean=FALSE, r
     # sort the time average in increasing order
     data.time.mean = sort(data.time.mean, decreasing=TRUE)
     
-    attr(data.time.mean, "specie") = species
-    attr(data.time.mean, "time.mean") = time.mean
-    attr(data.time.mean, "repl.mean") = repl.mean
     class(data.time.mean) = c("osmose.output.dietMatrix", class(data.time.mean))
     return(data.time.mean)
     
@@ -65,68 +52,40 @@ getVar.osmose.output.dietMatrix = function(out, species=NULL, time.mean=FALSE, r
   index = sort(temp, decreasing=FALSE, index.return=TRUE)$ix
   # returns the sorted array
   out = out[, index]
-  attr(out, "specie") = species
-  attr(out, "time.mean") = time.mean
-  attr(out, "repl.mean") = repl.mean
+  
   class(out) = c("osmose.output.dietMatrix", class(out))
   
   return(out)
 }
 
 
-#' @param ... Additional arguments of the function.
-#' @return An array or a list containing the data.
-#' @export
-#' @method getVar osmose.output.mortalityRate
-getVar.osmose.output.mortalityRate = function(out, species=NULL, time.mean=FALSE, repl.mean=TRUE, ...)
+# @param ... Additional arguments of the function.
+# @return An array or a list containing the data.
+process.mortalityRate = function(out, species=NULL, time.mean=FALSE, ...)
 {
   
   .check_species(out, species)
   
-  if(!repl.mean) {
-    warning("The repl.mean argument has been set to True")
-    repl.mean = TRUE
-  }
-  
   # extract the given specie
   out = out[[species]]
   
-  if(repl.mean)
-  {
-    # computes the replicate mean: for each list element
-    # computes the mean over the 4th dimension (replicate)
-    out = lapply(out, apply, c(1, 2, 3), mean)
-    
-    # removes the spurious dimension for each list element:
-    # dimension is now ntime, 1, 5 and becomes ntime, 5
-    out = lapply(out, drop)
-  }
+  # computes the replicate mean: for each list element
+  # computes the mean over the 4th dimension (replicate)
+  out = lapply(out, apply, c(1, 2), mean)
   
+  # if time.mean, computes the time average
   if(time.mean)
   {
     # for each element, compute the time-average of the matrix
     out = lapply(out, apply, 2, mean)
   }
   
-  attr(out, "specie") = species
-  attr(out, "time.mean") = time.mean
-  attr(out, "repl.mean") = repl.mean
+  
   class(out) = c("osmose.output.mortalityRate", class(out))
   
   return(out)
   
 }
-
-
-
-
-
-
-
-
-
-
-
 
 
 .check_species = function(out, species)
@@ -146,7 +105,7 @@ getVar.osmose.output.mortalityRate = function(out, species=NULL, time.mean=FALSE
 
 .compute_average_matrix = function(out, time.mean, repl.mean)
 {
-
+  
   species = colnames(out)
   
   # If replicate and time average: average over dims 2 and 3
