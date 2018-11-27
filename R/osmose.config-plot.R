1eb59ea18e78b91cbd77b5cb4fc0092791481159
CriscelyLP <criscelylujan@gmail.com>
2018-11-27 17:30:29 +0100

    plot.osmose.config.reproduction: new function for configuration class

--- R/osmose.config-plot.R
+++ R/osmose.config-plot.R
@@ -1,4 +1,14 @@
 
-plot.osmose.config.reproduction = function(){
+plot.osmose.config.reproduction = function(x, species = NULL, start = NULL, end = NULL, initialYear = NULL, freq = 12,
+                                           ts = TRUE, type = 1, xlim =NULL, ylim = NULL,
+                                           speciesNames = NULL, axes = TRUE, legend = TRUE, ...){
   
+  
+  osmoseConfigPlot2D(x = x, species = species, start = start, end = end,
+                     initialYear = initialYear, freq = freq,
+                     ts = ts, type = type, xlim = xlim, ylim = ylim,
+                     speciesNames = speciesNames, axes = axes, legend = legend, ...)
+  
+  return(invisible())
 }
+
