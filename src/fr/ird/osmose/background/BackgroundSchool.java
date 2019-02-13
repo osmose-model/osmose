4a29b66a4f765ee2973b2406365a0c848af0cd6b
barrier.n <barrier.n@45b28200-7c74-4d1a-96bc-342942c5875a>
2019-02-13 14:11:37 +0000

    Remove exception (can be used for bkg species since they feed)
    git-svn-id: https://subversion.renater.fr/authscm/barrier.n/svn/osmose/branches/alaia_bioen@1586 45b28200-7c74-4d1a-96bc-342942c5875a

--- src/fr/ird/osmose/background/BackgroundSchool.java
+++ src/fr/ird/osmose/background/BackgroundSchool.java
@@ -149,7 +149,7 @@ public class BackgroundSchool extends AbstractSchool {
 
     @Override
     public void incrementIngestion(double cumPreyUpon) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
