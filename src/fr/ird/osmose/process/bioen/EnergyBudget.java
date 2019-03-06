92bad0e5bbfa4fb6c29f2b07fc0d190d8c9d991e
barrier.n <barrier.n@45b28200-7c74-4d1a-96bc-342942c5875a>
2019-03-06 13:56:18 +0000

    Remove allometric relationships (already implemented in School.java) Use of the new maturity variables. Use of school increment methods (for weight and gonad weight) Correction of a bug in the calculation of the F_K function (equation 10)
    git-svn-id: https://subversion.renater.fr/authscm/barrier.n/svn/osmose/branches/alaia_bioen@1596 45b28200-7c74-4d1a-96bc-342942c5875a

--- src/fr/ird/osmose/process/bioen/EnergyBudget.java
+++ src/fr/ird/osmose/process/bioen/EnergyBudget.java
@@ -7,7 +7,6 @@ package fr.ird.osmose.process.bioen;
 
 import fr.ird.osmose.School;
 import fr.ird.osmose.process.AbstractProcess;
-import fr.ird.osmose.process.bioen.PhysicalData;
 
 /**
  *
@@ -21,24 +20,18 @@ public class EnergyBudget extends AbstractProcess {
 
     private double[] alpha;
     private double csmr;
-    
-    private double m0, m1,a_mat;
-    
-    
+
+    private double m0, m1;
+
     /**
      * Parameters for the energy maintenance.
      */
     private double c_t1, c_t2;
-    
+
     /**
      * Parameters for the kappa function.
      */
-    private double r,growth_pot;
-    
-    /**
-     * Parameters for the allometric relationship between L and W
-     */
-    private double p1, p2;
+    private double r, growth_pot;
 
     PhysicalData temperature_input;
 
@@ -48,58 +41,48 @@ public class EnergyBudget extends AbstractProcess {
         temperature_input = new PhysicalData(rank, "temperature");
 
     }
-    
-    
+
     @Override
     public void init() {
         
         String key;
-        
+
         key = "bioen.gross.energy.tmin";
         tmin = getConfiguration().getDouble(key);
-        
+
         key = "bioen.gross.energy.tmax";
         tmax = getConfiguration().getDouble(key);
-        
+
         key = "bioen.gross.energy.topt";
         topt = getConfiguration().getDouble(key);
- 
+
         key = "bioen.arrh.ct1";
         c_t1 = getConfiguration().getDouble(key);
-        
+
         key = "bioen.arrh.ct2";
         c_t2 = getConfiguration().getDouble(key);
-        
+
         key = "bioen.maint.energy.csmr";
         csmr = getConfiguration().getDouble(key);
-        
+
         key = "bioen.maturity.m0";
         m0 = getConfiguration().getDouble(key);
 
         key = "bioen.maturity.m1";
         m1 = getConfiguration().getDouble(key);
-        
-        key = "bioen.maturity.p1";
-        p1 = getConfiguration().getDouble(key);
-        
-        key = "bioen.maturity.p2";
-        p2 = getConfiguration().getDouble(key);
-        
+
         key = "bioen.maturity.r";
         r = getConfiguration().getDouble(key);
-        
+
         key = "bioen.maturity.growth_pot";
         growth_pot = getConfiguration().getDouble(key);
-        
-        a_mat=-1; // Test value : while a_mat=-1, maturity has not been reached
-        
-        
+
         this.compute_abc();
-        
+
         // Redundant with the alpha of the BioenPredationMortality class.
         int nBack = this.getNBkgSpecies();
         int nspec = this.getNSpecies();
-        
+
         // Recovers the alpha coefficient for focal + background species
         alpha = new double[nspec + nBack];
         for (int i = 0; i < this.getNSpecies(); i++) {
@@ -110,12 +93,12 @@ public class EnergyBudget extends AbstractProcess {
         for (int i = 0; i < nBack; i++) {
             key = String.format("species.alpha.bkg%d", i);
             alpha[i + nspec] = this.getConfiguration().getDouble(key);
-        }   
-        
+        }
+
     }
-    
-     /** Computes the phiT coefficients function for gross energy.
-     * Equation 4 
+
+    /**
+     * Computes the phiT coefficients function for gross energy. Equation 4
      */
     public void compute_abc() {
         this.b = 2 / (topt * (tmin / topt - 1));
@@ -132,183 +115,152 @@ public class EnergyBudget extends AbstractProcess {
     public void run() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
-    /** Returns the phiT function for gross energy.
-     * Equation 4
+
+    /**
+     * Returns the phiT function for gross energy. Equation 4
+     *
      * @param school
-     * @return 
+     * @return
      */
     public double get_phiT(School school) {
-                
+
         // Recovers the temperature of the school cell
         double temp = temperature_input.getValue(school);
-        
+
         if ((temp <= tmin) || (temp > tmax)) {
             return 0;
         }
-        
+
         double output = (temp <= topt) ? this.a * Math.pow(temp, 2) + this.b * Math.pow(temp, 1) + this.c : this.ap * Math.pow(temp, 2) + this.bp * Math.pow(temp, 1) + this.cp;
         return output;
-        
+
     }
-    
-    /** Returns the Arrhenius function for a given school.
-     * Cf. equation 6
-     * 
+
+    /**
+     * Returns the Arrhenius function for a given school. Cf. equation 6
+     *
      * @param school
-     * @return 
+     * @return
      */
-    public double get_Arrhenius(School school) { 
-        
+    public double get_Arrhenius(School school) {
+
         // Recovers the temperature of the school cell
         double temp = temperature_input.getValue(school);
         return Math.exp(this.c_t1 - (this.c_t2 / temp));
-        
+
     }
-    
-    /** Computes the maintenance coefficient. 
-     * Equation 5
+
+    /**
+     * Computes the maintenance coefficient. Equation 5
+     *
      * @param school
-     * @return 
+     * @return
      */
     public double get_maintenance(School school) {
-        
+
         int ispec = school.getSpeciesIndex();
         double output = this.csmr * Math.pow(school.getBiomass(), alpha[ispec]) * this.get_Arrhenius(school);
         return output;
-                
+
     }
-    
-    /** Returns the gross energy. 
-     * Equation 3
+
+    /**
+     * Returns the gross energy. Equation 3
+     *
      * @param school
-     * @return 
+     * @return
      */
-    public double get_egross(School school) { 
+    public double get_egross(School school) {
         return school.getIngestion() * this.get_phiT(school);
     }
-    
-    /** Returns the net energy. 
-     * Equation 7
+
+    /**
+     * Returns the net energy. Equation 7
+     *
      * @param school
-     * @return 
+     * @return
      */
-    public double get_enet(School school) { 
+    public double get_enet(School school) {
         return this.get_egross(school) - this.get_maintenance(school);
     }
-    
-    /** Determines the maturity state. 
-     * Equation 8
+
+    /**
+     * Determines the maturity state. Equation 8
+     *
      * @param school
-     * @return 
+     * @return
      */
     public int get_maturation(School school) {
         
+        // If the school is mature, nothing is done and returns 1
+        if(school.isMature()) {
+            return 1;
+        }
+        
+        // If the school is not mature yet, maturation is computed following equation 8
         double age = school.getAge();  // returns the age in years
-        double length = school.getLength();     // warning: length in cm.
+        double length = school.getLength();   // warning: length in cm.
         double llim = this.m0 * age + this.m1;   // computation of a maturity
-        
+
         int output = (length >= llim) ? 1 : 0;
+        if(output == 1) {
+            school.setAgeMat(age);
+            school.setIsMature(true);
+        }
+        
         return output;
-        
-    }
-    
-    /** Returns the somatic weight increment 
-     * Equation 11
-     * @param school
-     * @return 
-     */
-     public double get_dw(School school){
-        double dgrowth = (this.get_enet(school) >= 0) ? this.get_enet(school) : 0;
-        return(dgrowth*this.get_kappa(school));     
-    }
-    
-     /** Returns the gonadic weight increment 
-     * Equation 12
-     * @param school
-     * @return 
-     */
-     
-    public double get_dg(School school){
-        double gonadWeight = school.getGonadWeight(); // il faut créer une variable d'état g
-        double dg = (this.get_enet(school) > -gonadWeight) ? this.get_enet(school)*this.get_kappa(school) : -gonadWeight;
-        // If Enet positive, g pool increases. If Enet is negative, g is a reserve until g=0. 
-        return(dg);     
-    }
-      
-    /** Returns the proportion of net energy allocated to somatic growth 
-     * Equation 10'
-     * @param school
-     * @return 
-     */
-     
-    public double get_kappa(School school) { 
-        double age_mature = get_Age_maturation(school);  // Obtain the maturation state 
-        double age = school.getAge();  // returns the age in years
-        double kappa = (age <= age_mature) ? 1 : 1-(r/growth_pot)*school.getWeight(); //Function in two parts according to maturity state
-        return kappa;
-    }
-    /** Returns dL, the increment of length
-     * Allometric relationship between w and L
-     * @param school
-     * @return 
-     */
-    public double get_dLength(School school) {
-        double dw = get_dw(school);
-        double weigth = school.getWeight();
-        double length = school.getLength();
-        double length_t_1 = p1*(weigth+dw)*(p2);
-        return (length_t_1-length);       
-    }
-       
-    /** Returns the maturation age when it is reached, -1 otherwise
-     * @param school
-     * @return 
-     */
-    
-    public double get_Age_maturation(School school) {
-        double age = school.getAge();  // returns the age in years
-        double length  = school.getLength();  // returns the length in cm at the beginning of dt   
-        double dL = this.get_dLength(school); // returns the increment dL
-        double age_lim = ((length + dL)-this.m1)/this.m0;  // Returns recquired age to be mature with the current size + dL
-        double mat = get_maturation(school); // Returns the maturity state at the beginning of the steptime
-        double mat_t_1 = (age >= age_lim) ? 1 : 0; // Returns the maturity state at the end of the steptime
-        return  (this.a_mat == -1) ? ((mat > mat_t_1) ? age : this.a_mat) : this.a_mat; 
-        // If the maturity state changes during the timestep, a_mat become equal to the current age
-    }
-    /** Computes the maturation age
-     * @param a_mat
-     */
-    public void set_Age_maturation(double a_mat){
-        this.a_mat = a_mat;
-    }
-    
-    /** Computes the somatic weight increment
-     * @param dw
-     * @param school 
-     */
-    public void incrementWeight(float dw, School school) {
-        float weight = school.getWeight();
-        float newWeight = weight + dw;          
-        school.setWeight(newWeight);
-        
-    }
-    /** Computes the gonadic weight increment
-     * @param dg
-     * @param school 
-     */
-    public void incrementGonadWeight(float dg, School school) {
-        float gonadWeight = school.getGonadWeight();
-        float newGonadWeight = gonadWeight + dg;          
-        school.setGonadWeight(newGonadWeight);
+
     }
 
-    /** Tests the maturity state
-     * @param school 
-     * @return  
+    /**
+     * Returns the somatic weight increment (Equation 11).
+     *
+     * @param school
+     * @return
      */
-    public boolean isSexuallyMature(School school) {
-        return (get_Age_maturation(school) != -1);
+    public void get_dw(School school) {
+        
+        // computes the trend in structure weight dw/dt
+        double dgrowth = (this.get_enet(school) >= 0) ? this.get_enet(school) : 0;
+
+        // increments the weight
+        school.incrementWeight((float) dgrowth);
     }
-    
+
+    /**
+     * Returns the gonadic weight increment Equation 12
+     *
+     * @param school
+     * @return
+     */
+    public void get_dg(School school) {
+        double trend = 0;
+        double enet = this.get_enet(school);
+        if(enet > 0) {
+            trend = (1 - this.get_kappa(school)) * enet;
+        } else {
+            // if enet is negative, gonad weight is removed
+            // all the g is removed if enet is greater in absolute than g
+            trend = Math.max(enet, -school.getGonadWeight());
+        }
+        
+        school.incrementGonadWeight((float)trend);
+        
+    }
+
+    /**
+     * Returns the proportion of net energy allocated to somatic growth (equation
+     * 10').
+     *
+     * @param school
+     * @return
+     */
+    public double get_kappa(School school) {
+        int ispec = school.getSpeciesIndex();
+        // If the organism is imature, all the net energy goes to the somatic growth.
+        // else, only a kappa fraction goes to somatic growth
+        double kappa = (!school.isMature()) ? 1 : 1 - (r / growth_pot) * Math.pow(school.getWeight(), 1 - alpha[ispec]); //Function in two parts according to maturity state
+        return kappa;
+    }
+
 }
