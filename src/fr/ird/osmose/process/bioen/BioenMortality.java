02c3e625a2e0680925c36b5f93990c597f3946e6
barrier.n <barrier.n@45b28200-7c74-4d1a-96bc-342942c5875a>
2019-03-07 10:19:50 +0000

    Adding a module that manages the mortality for the bioen module (oxydation, starvation)
    git-svn-id: https://subversion.renater.fr/authscm/barrier.n/svn/osmose/branches/alaia_bioen@1601 45b28200-7c74-4d1a-96bc-342942c5875a

---
+++ src/fr/ird/osmose/process/bioen/BioenMortality.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.ird.osmose.process.bioen;
+
+import fr.ird.osmose.School;
+import fr.ird.osmose.process.AbstractProcess;
+
+/**
+ *
+ * @author nbarrier
+ */
+public class BioenMortality extends AbstractProcess  {
+
+    private double k_dam;
+
+    public BioenMortality(int rank) {
+        super(rank);
+    }
+
+    public void init() {
+
+        String key = "bioen.damage.k_dam";
+        k_dam = getConfiguration().getDouble(key);
+    
+    }
+
+    public double compute_oxydative_mort(School school) {
+        // calculation of PhiT
+        return this.k_dam * school.getEGross();
+    }
+    
+    public double compute_starv_mort(School school) {
+        
+        double output = 0;
+        double enet = school.getENet();
+        
+        if(school.getGonadWeight() >= -enet) {
+            output = 0;
+        }
+        
+        else {
+            
+            // Calculation of the number of dead individuals (equation 17)
+            double ndead = enet / school.getWeight();
+            
+            output = ndead * school.getWeight() / school.getInstantaneousBiomass();
+            
+            
+        }
+        
+        return output;
+        
+    }
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
