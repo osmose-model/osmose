4ee596a237b8726eca49a1b5422fc805f003dae8
amorell <amorell@45b28200-7c74-4d1a-96bc-342942c5875a>
2019-03-14 15:06:22 +0000

    Le module génétique est ajouté. Dans cette version, deux classes de processus ont été créée. La première classe "CreatedGenotype.java" est utilisée pour générer aléatoirement un génotype pour un individu d'une espèce donnée à l'initialisation.
    La seconde classe "NewBornGenotype.java" est utilisée quand un nouvel oeuf est pondu. Elle tire deux parents aléatoirement pour cet oeuf et génère le génotype de cet oeuf. 
    
    A faire : il faut maintenant ajouter un génotype à chaque school. La fonction getGenotype a été crée dans School pour faire compiler le code, mais est actuellement vide. 
    
    git-svn-id: https://subversion.renater.fr/authscm/barrier.n/svn/osmose/branches/alaia_bioen@1605 45b28200-7c74-4d1a-96bc-342942c5875a

---
+++ src/fr/ird/osmose/process/genet/NewBornGenotype.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.ird.osmose.process.genet;
+
+import fr.ird.osmose.School;
+import fr.ird.osmose.process.AbstractProcess;
+
+/**
+ *
+ * @author amorell
+ */
+
+/**
+ * This class is used to simulate meiosis and the formation of a new offspring 
+ * and the random drawn of two parents. It is applied to a new egg. 
+ * 
+ */
+public class NewBornGenotype extends AbstractProcess {
+
+    private int Lc;
+    private int nEvolvingParam;
+    private int iSpec;
+    
+    private School school;
+       
+        
+    public NewBornGenotype(int rank,School school) {
+        super(rank);
+    }
+
+    @Override
+    public void init() {
+        String key;
+        
+        key = "genet.Lc";
+        Lc = getConfiguration().getInt(key);
+        
+        iSpec=school.getSpeciesIndex();
+    }
+
+    @Override
+    public void run() {
+
+        int[][][] Genotype_parent1=ParentSelection(this.iSpec).getGenotype();
+        int[][][] Genotype_parent2=ParentSelection(this.iSpec).getGenotype();
+        meiosis(Genotype_parent1,Genotype_parent2);
+    }
+        
+
+    
+    private int[][][] meiosis(int[][][] Genotype_parent1, int[][][] Genotype_parent2) {
+        int[][][] Genotype_NewBorn;
+        Genotype_NewBorn=new int[nEvolvingParam][Lc][2];
+        for (int i = 0; i < this.nEvolvingParam; i++) {
+            for (int j = 0; j < Lc; j++) {    
+            Genotype_NewBorn[i][j][0]=Genotype_parent1[i][j][(int)(Math.random()*2)]; 
+            Genotype_NewBorn[i][j][1]=Genotype_parent2[i][j][(int)(Math.random()*2)]; 
+        }
+       }
+        return Genotype_NewBorn;
+    }
+    
+    
+    private double getGonadWeight_species(int iSpec){
+        // loop over all the schools of the species iSpec to compute the 
+        // specific g 
+        double g_species = 0;        
+        for (School school : getSimulation().getSchoolSet().getSchools(getSpecies(iSpec))) {
+            g_species=+school.getGonadWeight();
+        }
+        return g_species;
+    }
+       
+    private School ParentSelection(int iSpec){
+        // loop to calcul the probabilities to be parent of each school 
+        double[] g_species; 
+        int i = 0;
+        g_species = new double [iSpec];
+        double g_tot=getGonadWeight_species(iSpec);
+        for (School school : getSimulation().getSchoolSet().getSchools(getSpecies(iSpec))) {
+            if(i==0){
+                g_species[i]=school.getGonadWeight()/g_tot;
+            }
+            
+            else{
+                g_species[i]=(school.getGonadWeight()+g_species[i-1])/g_tot;
+            }
+            i=+1;
+        }
+        
+        // Random drawn of a parent
+        double parent=Math.random();
+        int j = 0;
+        while (parent>g_species[j]){
+        j=+1;
+        }
+        
+        return getSimulation().getSchoolSet().getSchools(getSpecies(iSpec)).get(j);
+    }    
+}
+   
