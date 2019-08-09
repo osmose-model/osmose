d4bb152f6c328713eaf0c0d1f1d53eab49c30bb6
barrier.n <barrier.n@45b28200-7c74-4d1a-96bc-342942c5875a>
2019-08-09 12:20:16 +0000

    Update de physical forcing file
    git-svn-id: https://subversion.renater.fr/authscm/barrier.n/svn/osmose/branches/alaia_bioen@1693 45b28200-7c74-4d1a-96bc-342942c5875a

--- NS_Bioen/master/make_temp_o2_northsea.py
+++ NS_Bioen/master/make_temp_o2_northsea.py
@@ -2,11 +2,40 @@ import xarray as xr
 import numpy as np
 import pylab as plt
 
-data = xr.open_dataset("/home/nbarrier/Bureau/Morgane_Alaia/LTL.nc")
-data = data.isel(ltl=slice(0, 3))
-data = data.rename({'ltl_biomass':'T'})
-data = data.rename({'ltl':'zindex'})
+data = xr.open_dataset("corrected_ns_ltlbiomassTons_test.nc")
+diatoms = data['Diatoms'].values
+time = data['time'].values
+latitude = data['latitude'].values
+longitude = data['longitude'].values
 
+ntime, nlat, nlon = diatoms.shape
+
+diatoms = diatoms[:, np.newaxis, :, :]
+
+T = np.ones((ntime, 3, nlat, nlon)) * 8.4
+O2 = np.ones((ntime, 3, nlat, nlon)) * 350.
+
+T = T * (np.isnan(diatoms) == False)
+O2 = O2 * (np.isnan(diatoms) == False)
+
+T[T==0] = np.nan
+O2[O2==0] = np.nan
+
+#data = data.rename({'ltl_biomass':'T'})
+#data = data.rename({'ltl':'zindex'})
+
+print(T.shape)
+print(latitude.shape)
+print(longitude.shape)
+
+ds = xr.Dataset({'T': (['time', 'zindex', 'y', 'x'], T),
+                  'O2': (['time', 'zindex', 'y', 'x'], O2)},
+                 coords={'lon': (['x'], latitude),'lat': (['y'], longitude)}
+                  )
+
+ds.to_netcdf("physical.nc", format="NETCDF4_CLASSIC")
+
+quit()
 
 mask = (np.isnan(data['T'].values) == False)
 data['O2'] = data['T']
