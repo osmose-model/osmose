126d2310dce24ec88f51938aa32da27ac21e901f
barriern <nicolas.barrier@ird.fr>
2020-03-12 17:54:30 +0100

    Updates of the mortality rate by ... to be consistent with classical mort. rates.

--- R/osmose-plot4d.R
+++ R/osmose-plot4d.R
@@ -19,12 +19,14 @@ plot.osmose.mortalityRate = function(x, species=NULL, speciesNames=NULL, norm=TR
   if(is.null(speciesNames)){
     speciesNames = toupper(names(x))
   }
-
+  
+  # computes the time and replicate mean for all the mortalities type class (eggs, juveniles, ...)
   outlist = list()
   for (sp in names(x)) {
     outlist[[sp]] = process.mortalityRate(x, time.mean=TRUE, species=sp)
   }
   
+  # draws plots
   msg = sprintf("3D plot type %d is not implemented yet.", type)
   switch(type,
          "1" = plotMortRateType1(outlist, norm=norm, speciesNames=speciesNames, ...),
@@ -35,39 +37,19 @@ plot.osmose.mortalityRate = function(x, species=NULL, speciesNames=NULL, norm=TR
   
 }
 
+# Plots the mortality rate as a stacked plot
 plotMortRateType1 = function(x, norm, speciesNames, ...) { 
   
   # To keep the plot params as the beginning
   op = par(no.readonly = TRUE)
   on.exit(par(op))
   
-  # Define multiplot array if there're more than 1 species
-  if(length(x) > 1){
-    mar = rep(4, 4)  # bottom left top right
-    mar = c(4, 4, 1, 4)
-    oma = rep(1, 4)
-    par(mar=mar, oma = oma)
-    mfrow = getmfrow(length(x))
-  } else {
-    mfrow = c(1, 1)
-  }
-  
-  par(mfrow = mfrow)
-  
   i = 1
-  for (data in x) { 
+  # Loop over all the species
+  for (data in x) {  
     
-    # if normalize, display mortality rates into percentage instead of absolutes.
-    if(norm) {
-      # apply the normalize function to all the elements of the list.
-      data = lapply(data, norm_func)
-    }
-    
-    # convert the list into a matrix
-    data = do.call(cbind.data.frame, data)
-    data = as.matrix(data)
-    print(data)
-    
+    data = .norm_final(data, norm=norm)
+
     xlabel = "Stage"
     if(norm) {
       ylabel = "Mortality rate (%)"
@@ -89,8 +71,7 @@ plotMortRateType1 = function(x, norm, speciesNames, ...) {
     }
     
     args$xaxt ="n"
-    print(dim(data))
-  
+
     xpos = do.call(barplot, args)
     axis(1, at=xpos, labels=colnames(data), las=2, cex.axis=1)  # below
     
@@ -105,7 +86,7 @@ plotMortRateType1 = function(x, norm, speciesNames, ...) {
     
     mtext(text = speciesNames[i], side = 3, line = -1.5, adj = 0.05, cex = cex)
     i = i + 1
-  
+    
   }
   
   return(invisible())
@@ -118,11 +99,9 @@ plotMortRateType2 = function(x, speciesNames, ...) {
   op = par(no.readonly = TRUE)
   on.exit(par(op))
   
-  par(mfrow = c(2, 2))
-  
   # recovers the list of arguments 
   args = list(...)
- 
+  
   # Sets the colors by using the color function.
   if(is.null(args$col)) {
     colfun = rainbow
@@ -131,13 +110,22 @@ plotMortRateType2 = function(x, speciesNames, ...) {
     args$col = NULL
   }
   
-  i = 1
+  i = 1  # species counter
+  
+  # loop over the species to extract 
   for (data in x) { 
-
-    for (class in names(data)) {
+    
+    # normalise mortality and convert it into a common feature
+    data = .norm_final(data, norm=FALSE)  
+    rnames = rownames(data)
+    cnames = colnames(data)
+    
+    # loop over the class (eggs, juveniles, adults)
+    for (class in colnames(data)) {
       
-      temp = data[[class]]
+      temp = data[, class] # extracts the data for the given species
       title = paste(speciesNames[i], class, sep=', ')
+      names(temp) = rnames
       
       args = list(...)
       args$x = temp
@@ -153,14 +141,97 @@ plotMortRateType2 = function(x, speciesNames, ...) {
              fill=args$col, bg="white")
       
     }
-    
-    # Add a spurious plot in order to have one page per species
-    plot.new()
-    
-    i = i + 1
   
+    i = i + 1
+    
   }
   
   return(invisible())
   
 }
+
+
+
+
+
+
+
+
+#' Plots mortality rates by size class.
+#'
+#' @param x Mortality object
+#' @param species Species name
+#' @param time.mean If time-average should be plotted. If TRUE, mortality rates by
+#' for each age class and for each mortality types are plotted. Else, the time series for
+#' a specific type of mortality is plotted. 
+#' @param norm Wheter mortality should be normalized (only if time.mean=TRUE)
+#' @param mtype Mortality type ("Mtot", Mpred", "Mstar", "Mnat", "F" or "Z")
+#' @param ... Additional arguments of the function.
+#' 
+plot.mortalityRateDistrib = function(x, species=NULL, norm=TRUE, ...) {
+  
+  # extract the values for a given list of species
+  x = .extract_species_from_list(x, species)
+  x = lapply(x, .process_mort_rate_dis, norm)
+  
+  msg = sprintf("3D plot type %d is not implemented yet.", type)
+  switch(type,
+         "1" = plotMortRateType1(outlist, norm=norm, speciesNames=speciesNames, ...),
+         "2" = plotMortRateType2(outlist, speciesNames=speciesNames, ...),
+         stop(msg))
+  
+  return(invisible())
+
+}
+
+
+.process_mort_rate_dis = function(x, norm=TRUE) {
+  
+  x = lapply(x, apply, mean, MARGIN=2, na.rm=TRUE) # computes the mean over replicates and time
+  mort = do.call(cbind.data.frame, x)
+  mort = as.matrix(mort)
+  
+  # Counts the total mortality rate for each size class
+  tot = apply(mort, 1, sum, na.rm=TRUE)
+  
+  # Extracts the size class with mortality > 0
+  mort = mort[tot>0, ]
+  tot = tot[tot>0]
+  
+  # If data should be normalized, then it is divided by the total
+  # mortality
+  if(norm) mort = (mort / tot) * 100
+  
+  # Transpose the dataframe
+  mort = as.data.frame(t(mort))
+  
+  
+}
+
+# Final normalization. For all the mortality class (either string or values)
+# normalize if needed (extracts percentage).
+
+.norm_final = function(data, norm) {
+  
+  isdf = is.data.frame(data)  # check if data is data frame
+  
+  # if data frame, recover the dimension names
+  dnames = NULL 
+  if(isdf) dnames = dimnames(data)
+  
+  # if normalize, display mortality rates into percentage instead of absolutes.
+  if(norm) {
+    # apply the normalize   function to all the elements of the list.
+    data = lapply(data, norm_func)
+  }
+  
+  # convert the list into a matrix
+  data = do.call(cbind.data.frame, data)
+  data = as.matrix(data)
+  
+  if(isdf) dimnames(data) = dnames
+  
+  return(data)
+  
+}
+  
