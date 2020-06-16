d854c131a2bd7a9ac4df611c5def219d8fb25d38
barriern <nicolas.barrier@ird.fr>
2020-06-16 08:20:36 +0200

    Update default version to 4.3.0

--- R/osmose2R.R
+++ R/osmose2R.R
@@ -1163,7 +1163,7 @@ existOsmoseParameter = function(par, ..., keep.att=FALSE) {
 #'demo = osmose_demo(path="../", config="gog")
 #'
 #'# run the osmose model
-#'run_osmose(demo$config_file, parameters=NULL, output=NULL, version="3.3.3", 
+#'run_osmose(demo$config_file, parameters=NULL, output=NULL, version="4.3.0", 
 #'           options=NULL, verbose=TRUE, clean=TRUE)
 #'
 #'# reads output data
