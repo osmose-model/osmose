3a8d9b7c3357a10490f76190c77ef9ae8a1a2ef8
barriern <nicolas.barrier@ird.fr>
2021-03-09 16:05:50 +0100

    Move iSpecPrey in block

--- java/src/main/java/fr/ird/osmose/process/mortality/PredationMortality.java
+++ java/src/main/java/fr/ird/osmose/process/mortality/PredationMortality.java
@@ -269,7 +269,6 @@ public class PredationMortality extends AbstractMortality {
         double[] percentResource = getPercentResource(predator);
 
         for (int iPrey = 0; iPrey < preys.size(); iPrey++) {
-            int iSpecPrey = preys.get(iPrey).getSpeciesIndex(); // get species index with offset
             IAggregation prey = (IAggregation) preys.get(iPrey);
             int iAccessPrey = accessibilityMatrix.getIndexPrey(prey);
             // The prey is an other school
@@ -283,6 +282,7 @@ public class PredationMortality extends AbstractMortality {
                     accessibility[iPrey] = 0.d; // no need to do it since initialization already set it to zero
                 }
             } else {
+                int iSpecPrey = preys.get(iPrey).getSpeciesIndex(); // get species index with offset
                 // The prey is a resource group
                 accessibility[iPrey] = accessibilityMatrix.getValue(iAccessPrey, iAccessPred)
                         * percentResource[iSpecPrey - nSpecies];
