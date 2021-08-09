30f1c89251082f582af6721b2beb98d6bfb717c9
barriern <nicolas.barrier@ird.fr>
2021-08-09 11:33:37 +0200

    Adding testing of indexes of pred access matrix

--- java/src/test/java/fr/ird/osmose/TestPredMatrix.java
+++ java/src/test/java/fr/ird/osmose/TestPredMatrix.java
@@ -18,11 +18,18 @@ import fr.ird.osmose.util.Matrix;
 public class TestPredMatrix {
     
     private Configuration cfg;
-    
     private Matrix accessMatrix;
+    private float x = 0.f;
+    private float y = 0.f;
+    private double abundance = 100;
+    private float length = 20;
+    private float weight = 20;
+    private float tl = 2;
+    
+    private int nStepYears;
     
     @Test
-    public void Test1() {
+    public void TestArrayDim() {
         
         int nPreys = accessMatrix.getNPrey();
         int nPred =  accessMatrix.getNPred();
@@ -35,7 +42,63 @@ public class TestPredMatrix {
         
     }
     
+    @Test
+    public void TestPredIndex() {
+        
+        double age;
+        Species species;
+        School school;
+        System.out.println("+++++++++++++++++++++ ");
+        for (int i = 0; i < Osmose.getInstance().getConfiguration().getNSpecies(); i++) {
+            System.out.println(this.getSpecies(i).getName());
+        }
+        
+        // testing species 0=lesserSpottedDogfish;
+        species = this.getSpecies(0);
+        age = 0.2;
+        school = this.getSchool(species, age);
+        assertEquals(0, this.accessMatrix.getIndexPred(school));
+
+        age = 0.45;
+        school = this.getSchool(species, age);
+        assertEquals(1, this.accessMatrix.getIndexPred(school));
+        
+        age = 0.46;
+        school = this.getSchool(species, age);
+        assertEquals(1, this.accessMatrix.getIndexPred(school));
+        
+        // testing for horseMackerel 18
+        species = this.getSpecies(9);
+        age = 0.1;
+        school = this.getSchool(species, age);
+        assertEquals(18, this.accessMatrix.getIndexPred(school));
+        
+        age = 1e6f;
+        school = this.getSchool(species, age);
+        assertEquals(18, this.accessMatrix.getIndexPred(school));
+
+        // testing for species= (squids) 22
+        species = this.getSpecies(13);
+        age = 0.1;
+        school = this.getSchool(species, age);
+        assertEquals(22, this.accessMatrix.getIndexPred(school));
+        
+        age = 0.2;
+        school = this.getSchool(species, age);
+        assertEquals(23, this.accessMatrix.getIndexPred(school));
+        
+    }
     
+    public School getSchool(Species species, double age) {
+        int ageDt = (int) Math.round(age * this.nStepYears);
+        School school = new School(species, x, y, abundance, length, weight, ageDt, tl, 0);
+        return school;
+    }
+    
+    public Species getSpecies(int index) {
+        return Osmose.getInstance().getConfiguration().getSpecies(index); 
+    }
+
     @BeforeAll
     public void prepareData() throws Exception {
         
@@ -52,10 +115,12 @@ public class TestPredMatrix {
         osmose.readConfiguration(configurationFile, cmd);
         cfg = osmose.getConfiguration();
         cfg.init();
-        AccessibilityManager access = new AccessibilityManager(0, "predation.accessibility", ".acc", (school -> (0)), false);
+        AccessibilityManager access = new AccessibilityManager(0, "predation.accessibility", ".acc", (school -> (school.getAge())), false);
         access.init();
         
         accessMatrix = access.getMatrix().get(-1);
+        
+        this.nStepYears = cfg.getNStepYear();
         
     }
     
