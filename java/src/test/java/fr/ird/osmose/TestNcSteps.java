5e142a3ed822df736bcddb6ab4ccab71409be69b
barriern <nicolas.barrier@ird.fr>
2021-09-21 15:49:21 +0200

    adding tests for indexing

--- java/src/test/java/fr/ird/osmose/TestNcSteps.java
+++ java/src/test/java/fr/ird/osmose/TestNcSteps.java
@@ -1,5 +1,7 @@
 package fr.ird.osmose;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.logging.Level;
@@ -40,6 +42,7 @@ public class TestNcSteps {
         }
     }
     
+    /** Test of a seasonal file, which contains one value per osmose dt **/
     @Test
     public void TestNcSteps1() throws IOException, InvalidRangeException {
         
@@ -47,8 +50,140 @@ public class TestNcSteps {
         String filename = this.createSingleLtlFile("test1", "test1_ltl_", 24);
         ForcingFile forcingFile = new ForcingFile("ltl", filename, 24, 0.0, 1.0, ForcingFileCaching.NONE);
         forcingFile.init();
+        
+        // Check the proper conversion from Osmose time-step to NetCDf time step
+        assertEquals(0, forcingFile.getNcStep(0));
+        assertEquals(1, forcingFile.getNcStep(1));
+        assertEquals(0, forcingFile.getNcStep(24));
+        assertEquals(0, forcingFile.getNcStep(48));
+        
+        assertEquals(3, forcingFile.getNcStep(1203));
+        assertEquals(8, forcingFile.getNcStep(2648));
+        
+        // Check the proper conversion from **global** netcdf index to individual ones
+        assertEquals(0, forcingFile.getNcIndex(0));
+        assertEquals(3, forcingFile.getNcIndex(3));
+        assertEquals(8, forcingFile.getNcIndex(8));
+        
+        // Check the proper conversion from **global** netcdf index to individual ones
+        assertEquals(filename, forcingFile.getNcFileName(0));
+        assertEquals(filename, forcingFile.getNcFileName(3));
+        assertEquals(filename, forcingFile.getNcFileName(8));
+        
+        File tempFile = new File(filename);
+        boolean deleted = tempFile.delete();
+        
     }
 
+        /** Test of a seasonal file, which contains one value per osmose dt **/
+        @Test
+        public void TestNcSteps2() throws IOException, InvalidRangeException {
+            
+            // Resource files has 24 steps per years
+            String filename = this.createSingleLtlFile("test2", "test2_ltl_", 12);
+            ForcingFile forcingFile = new ForcingFile("ltl", filename, 12, 0.0, 1.0, ForcingFileCaching.NONE);
+            forcingFile.init();
+            
+            // Check the proper conversion from Osmose time-step to NetCDf time step
+            assertEquals(0, forcingFile.getNcStep(0));
+            assertEquals(0, forcingFile.getNcStep(1));
+            assertEquals(0, forcingFile.getNcStep(24));
+            assertEquals(0, forcingFile.getNcStep(48));
+            
+            assertEquals(1, forcingFile.getNcStep(1203));
+            assertEquals(4, forcingFile.getNcStep(2648));
+            
+            // Check the proper conversion from **global** netcdf index to individual ones
+            assertEquals(0, forcingFile.getNcIndex(0));
+            assertEquals(3, forcingFile.getNcIndex(3));
+            assertEquals(8, forcingFile.getNcIndex(8));
+            
+            // Check the proper conversion from **global** netcdf index to individual ones
+            assertEquals(filename, forcingFile.getNcFileName(0));
+            assertEquals(filename, forcingFile.getNcFileName(3));
+            assertEquals(filename, forcingFile.getNcFileName(8));
+            
+            File tempFile = new File(filename);
+            boolean deleted = tempFile.delete();
+            
+        }
+        
+        /** Test of a seasonal file, which contains 5 years of monthly values **/
+        @Test
+        public void TestNcSteps3() throws IOException, InvalidRangeException {
+            
+            // Resource files has 24 steps per years
+            String filename = this.createSingleLtlFile("test3", "test3_ltl_", 5*12);
+            ForcingFile forcingFile = new ForcingFile("ltl", filename, 12, 0.0, 1.0, ForcingFileCaching.NONE);
+            forcingFile.init();
+            
+            // Check the proper conversion from Osmose time-step to NetCDf time step
+            assertEquals(0, forcingFile.getNcStep(0));
+            assertEquals(0, forcingFile.getNcStep(1));
+
+            assertEquals(16, forcingFile.getNcStep(32));
+            assertEquals(6, forcingFile.getNcStep(613));
+            assertEquals(3, forcingFile.getNcStep(1807));
+            
+            // Check the proper conversion from **global** netcdf index to individual ones
+            assertEquals(0, forcingFile.getNcIndex(0));
+            assertEquals(3, forcingFile.getNcIndex(3));
+            assertEquals(8, forcingFile.getNcIndex(8));
+            assertEquals(49, forcingFile.getNcIndex(49));
+            
+            // Check the proper conversion from **global** netcdf index to individual ones
+            assertEquals(filename, forcingFile.getNcFileName(0));
+            assertEquals(filename, forcingFile.getNcFileName(3));
+            assertEquals(filename, forcingFile.getNcFileName(8));
+            
+            File tempFile = new File(filename);
+            boolean deleted = tempFile.delete();
+            
+        }
+        
+     /** Test of a combination of three files with different number of time-steps **/
+     @Test
+     public void TestNcSteps4() throws IOException, InvalidRangeException {
+         
+         // Resource files has 24 steps per years
+         String filename1 = this.createSingleLtlFile("test4", "test4_a_ltl_", 12);
+         String filename2 = this.createSingleLtlFile("test4", "test4_b_ltl_", 2 * 12);
+         String filename3 = this.createSingleLtlFile("test4", "test4_c_ltl_", 3 * 12);
+         
+         String directory = new File(filename1).getParent();
+         String pattern = 
+         
+         System.out.println(directory);
+          
+         //ForcingFile forcingFile = new ForcingFile("ltl", filename, 12, 0.0, 1.0, ForcingFileCaching.NONE);
+         //forcingFile.init();
+         
+        //  // Check the proper conversion from Osmose time-step to NetCDf time step
+        //  assertEquals(0, forcingFile.getNcStep(0));
+        //  assertEquals(0, forcingFile.getNcStep(1));
+
+        //  assertEquals(16, forcingFile.getNcStep(32));
+        //  assertEquals(6, forcingFile.getNcStep(613));
+        //  assertEquals(3, forcingFile.getNcStep(1807));
+         
+        //  // Check the proper conversion from **global** netcdf index to individual ones
+        //  assertEquals(0, forcingFile.getNcIndex(0));
+        //  assertEquals(3, forcingFile.getNcIndex(3));
+        //  assertEquals(8, forcingFile.getNcIndex(8));
+        //  assertEquals(49, forcingFile.getNcIndex(49));
+         
+        //  // Check the proper conversion from **global** netcdf index to individual ones
+        //  assertEquals(filename, forcingFile.getNcFileName(0));
+        //  assertEquals(filename, forcingFile.getNcFileName(3));
+        //  assertEquals(filename, forcingFile.getNcFileName(8));
+         
+        //  File tempFile = new File(filename);
+        //  boolean deleted = tempFile.delete();
+         
+     }   
+        
+        
+        
     private String createSingleLtlFile(String directory, String prefix, int nSteps) throws IOException, InvalidRangeException {
          
         String fileName = File.createTempFile(prefix, ".nc").getAbsolutePath();
