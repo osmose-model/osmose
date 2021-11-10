940ec0f9d13d61180b93a7d32d7bed0c1c85ab3b
barriern <nicolas.barrier@ird.fr>
2021-11-10 14:53:26 +0100

    Adding files generated by use_rcpp

--- R/osmose-package.R
+++ R/osmose-package.R
@@ -90,3 +90,38 @@
 #' \href{https://documentation.osmose-model.org/index.html}{Documentation website}
 #' @keywords modelling marine ecosystems
 NULL
+
+## usethis namespace: start
+#' @useDynLib osmose, .registration = TRUE
+## usethis namespace: end
+NULL
+
+## usethis namespace: start
+#' @importFrom Rcpp sourceCpp
+## usethis namespace: end
+NULL
+
+#' @import graphics
+NULL
+#' @import grDevices
+NULL
+#' @importFrom stats median sd quantile setNames dlnorm dnorm qnorm
+NULL
+#' @importFrom utils read.csv read.table write.table tail write.csv
+NULL
+#' @importFrom rlist list.merge
+NULL
+#' @importFrom stringr str_split str_trim str_sub
+NULL
+#' @importFrom rmarkdown render
+NULL
+#' @importFrom knitr knit
+NULL
+#' @importFrom ncdf4 nc_open nc_close ncvar_put ncvar_get ncvar_put nc_create ncdim_def ncatt_get ncvar_def ncatt_put
+NULL
+#' @importFrom mgcv gam
+NULL
+#' @importFrom stats predict
+NULL
+#' @importFrom fields image.plot tim.colors
+NULL
