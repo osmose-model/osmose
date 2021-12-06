1ce9b975dc4cf4a9bc9d295f9264b33f688c3eda
barriern <nicolas.barrier@ird.fr>
2021-12-06 18:02:28 +0100

    init the class for the writting of access biomass

---
+++ java/src/main/java/fr/ird/osmose/output/FishingAccessBiomassOutput.java
@@ -0,0 +1,153 @@
+/* 
+ * 
+ * OSMOSE (Object-oriented Simulator of Marine Ecosystems)
+ * http://www.osmose-model.org
+ * 
+ * Copyright (C) IRD (Institut de Recherche pour le Développement) 2009-2020
+ * 
+ * Osmose is a computer program whose purpose is to simulate fish
+ * populations and their interactions with their biotic and abiotic environment.
+ * OSMOSE is a spatial, multispecies and individual-based model which assumes
+ * size-based opportunistic predation based on spatio-temporal co-occurrence
+ * and size adequacy between a predator and its prey. It represents fish
+ * individuals grouped into schools, which are characterized by their size,
+ * weight, age, taxonomy and geographical location, and which undergo major
+ * processes of fish life cycle (growth, explicit predation, additional and
+ * starvation mortalities, reproduction and migration) and fishing mortalities
+ * (Shin and Cury 2001, 2004).
+ * 
+ * Contributor(s):
+ * Yunne SHIN (yunne.shin@ird.fr),
+ * Morgane TRAVERS (morgane.travers@ifremer.fr)
+ * Ricardo OLIVEROS RAMOS (ricardo.oliveros@gmail.com)
+ * Philippe VERLEY (philippe.verley@ird.fr)
+ * Laure VELEZ (laure.velez@ird.fr)
+ * Nicolas Barrier (nicolas.barrier@ird.fr)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation (version 3 of the License). Full description
+ * is provided on the LICENSE file.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * 
+ */
+
+package fr.ird.osmose.output;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import fr.ird.osmose.School;
+import fr.ird.osmose.util.SimulationLinker;
+
+/**
+ *
+ * @author Nicolas Barrier
+ */
+public class FishingAccessBiomassOutput extends SimulationLinker implements IOutput {
+    
+    private FileOutputStream fos[];
+    private PrintWriter prw[];
+    private int recordFrequency;
+    /**
+     * CSV separator
+     */
+    private final String separator;
+    
+    FishingAccessBiomassOutput(int rank) {
+        super(rank);
+        separator = getConfiguration().getOutputSeparator();
+    }
+
+    
+    @Override
+    public void initStep() {
+        // TODO Auto-generated method stub
+        
+    }
+    @Override
+    public void reset() {
+        // TODO Auto-generated method stub
+        
+    }
+    @Override
+    public void update() {
+        // TODO Auto-generated method stub
+        
+    }
+    @Override
+    public void write(float time) {
+        // TODO Auto-generated method stub
+        
+    }
+    @Override
+    public boolean isTimeToWrite(int iStepSimu) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    @Override
+    public void init() {
+        fos = new FileOutputStream[getNSpecies()];
+        prw = new PrintWriter[getNSpecies()];
+        int nFisheries = getConfiguration().getNFishery();
+        
+        for (int iSpecies = 0; iSpecies < getNSpecies(); iSpecies++) {
+            // Create parent directory
+            File path = new File(getConfiguration().getOutputPathname());
+            StringBuilder filename = new StringBuilder("Econ");
+            filename.append(File.separatorChar);
+            filename.append(getConfiguration().getString("output.file.prefix"));
+            filename.append("_accessBiomass-");
+            filename.append(getSpecies(iSpecies).getName());
+            filename.append("_Simu");
+            filename.append(getRank());
+            filename.append(".csv");
+            File file = new File(path, filename.toString());
+            boolean fileExists = file.exists();
+            file.getParentFile().mkdirs();
+            try {
+                // Init stream
+                fos[iSpecies] = new FileOutputStream(file, true);
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(MortalityOutput.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            prw[iSpecies] = new PrintWriter(fos[iSpecies], true);
+            if (!fileExists) {
+                // Write headers
+                prw[iSpecies].print(quote("Time"));
+                prw[iSpecies].print(separator);
+                for (int iFishery = 0; iFishery < nFisheries - 1; iFishery++) { 
+                    String fishingName = getSimulation().getFishingGear(iFishery).getName();
+                    prw[iSpecies].print(quote(fishingName));
+                    prw[iSpecies].print(separator);
+                }
+                int iFishery = nFisheries - 1;
+                String fishingName = getSimulation().getFishingGear(iFishery).getName();
+                prw[iSpecies].print(quote(fishingName));
+                prw[iSpecies].println();
+            }
+            
+        } 
+    }
+    
+    @Override
+    public void close() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    private String quote(String str) {
+        return "\"" + str + "\"";
+    }
+}
