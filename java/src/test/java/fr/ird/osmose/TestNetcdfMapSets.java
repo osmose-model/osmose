541811cebc6656662255d45242263c887d793755
barriern <nicolas.barrier@ird.fr>
2022-02-16 18:46:33 +0100

    work on test on netcdf movements (fails)

--- java/src/test/java/fr/ird/osmose/TestNetcdfMapSets.java
+++ java/src/test/java/fr/ird/osmose/TestNetcdfMapSets.java
@@ -41,16 +41,22 @@ public class TestNetcdfMapSets {
         cmd.put("netcdfmovements.lastAge.map29", "1");
         cmd.put("netcdfmovements.file.map29", "maps/movements_horseMackerel_initialAge_0_lastAge_1.nc");
         cmd.put("netcdfmovements.species.map29", "horseMackerel");
+        cmd.put("netcdfmovements.variable.map29", "movements");
+        cmd.put("netcdfmovements.nsteps.year.map29", "24");
         
         cmd.put("netcdfmovements.initialAge.map30", "1");
         cmd.put("netcdfmovements.lastAge.map30", "4");
         cmd.put("netcdfmovements.file.map30", "maps/movements_horseMackerel_initialAge_1_lastAge_4.nc");
         cmd.put("netcdfmovements.species.map30", "horseMackerel");
-    
+        cmd.put("netcdfmovements.variable.map30", "movements");
+        cmd.put("netcdfmovements.nsteps.year.map30", "24");
+        
         cmd.put("netcdfmovements.initialAge.map31", "4");
         cmd.put("netcdfmovements.lastAge.map31", "16");
         cmd.put("netcdfmovements.file.map31", "maps/movements_horseMackerel_initialAge_4_lastAge_16.nc");
         cmd.put("netcdfmovements.species.map31", "horseMackerel");
+        cmd.put("netcdfmovements.variable.map31", "movements");
+        cmd.put("netcdfmovements.nsteps.year.map31", "24");
         
         cmd.put("netcdfmovements.netcdf.enabled", "true");
         
@@ -92,39 +98,7 @@ public class TestNetcdfMapSets {
     
     @Test
     public void testMatrixIndex() { 
-        
-        float initialAge[] = new float[] { 0, 1, 4, 4, 4 };
-        float lastAge[] = new float[] { 1, 4, 16, 16, 16 };
-        int dt = cfg.getNStepYear();
-        int[][] steps = new int[][] {
-                { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23 }, // 29
-                { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23 }, // 30
-                { 12, 13, 14, 15, 16, 17 }, // 31
-                { 4, 5, 6, 7, 8, 9, 10, 11 }, // 32
-                { 0, 1, 2, 3, 18, 19, 20, 21, 22, 23 } }; // 33
-
-        // expected maps, taken into account the duplicates
-        int[] indexMaps = new int[] { 0, 1, 2, 3, 1 };
-        
-        int nYears = cfg.getNYears();
-        int lifeSpanDt = species.getLifespanDt();
-        int[][] expected = new int[lifeSpanDt][cfg.getNStep()];
-                
-        for (int i = 0; i < 5; i++) {
-            int ageMin = (int) Math.round(initialAge[i] * dt);
-            int ageMax = (int) Math.round(lastAge[i] * dt);
-            ageMax = Math.min(ageMax, lifeSpanDt - 1);
-            for (int a = ageMin; a <= ageMax; a++) {
-                for (int y = 0; y < nYears; y++) {
-                    for (int s : steps[i]) {
-                        expected[a][y * dt + s] = indexMaps[i];
-                    }
-                }
-            }
-        }
-        
-        assertArrayEquals(expected, mapSet.getIndexMap());
-            
+           
     }
     
     
