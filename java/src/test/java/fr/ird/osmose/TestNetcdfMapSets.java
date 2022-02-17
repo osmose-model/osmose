e4c5c98a71bbb7f66db30330dc6c1dd21cb3ff2d
barriern <nicolas.barrier@ird.fr>
2022-02-17 11:03:30 +0100

    Set absolute paths in test + update file

--- java/src/test/java/fr/ird/osmose/TestNetcdfMapSets.java
+++ java/src/test/java/fr/ird/osmose/TestNetcdfMapSets.java
@@ -35,25 +35,26 @@ public class TestNetcdfMapSets {
         osmose.getLogger().setLevel(Level.SEVERE);
         
         String configurationFile = this.getClass().getClassLoader().getResource("osmose-eec/eec_all-parameters.csv").getFile();
+        String dirMaps = this.getClass().getClassLoader().getResource("osmose-eec/").getFile();
         
         // Overwrite some parameters
         cmd.put("netcdfmovements.initialAge.map29", "0");
         cmd.put("netcdfmovements.lastAge.map29", "1");
-        cmd.put("netcdfmovements.file.map29", "maps/movements_horseMackerel_initialAge_0_lastAge_1.nc");
+        cmd.put("netcdfmovements.file.map29", dirMaps + "maps/movements_horseMackerel_initialAge_0_lastAge_1.nc");
         cmd.put("netcdfmovements.species.map29", "horseMackerel");
         cmd.put("netcdfmovements.variable.map29", "movements");
         cmd.put("netcdfmovements.nsteps.year.map29", "24");
         
         cmd.put("netcdfmovements.initialAge.map30", "1");
         cmd.put("netcdfmovements.lastAge.map30", "4");
-        cmd.put("netcdfmovements.file.map30", "maps/movements_horseMackerel_initialAge_1_lastAge_4.nc");
+        cmd.put("netcdfmovements.file.map30", dirMaps + "maps/movements_horseMackerel_initialAge_1_lastAge_4.nc");
         cmd.put("netcdfmovements.species.map30", "horseMackerel");
         cmd.put("netcdfmovements.variable.map30", "movements");
         cmd.put("netcdfmovements.nsteps.year.map30", "24");
         
         cmd.put("netcdfmovements.initialAge.map31", "4");
         cmd.put("netcdfmovements.lastAge.map31", "16");
-        cmd.put("netcdfmovements.file.map31", "maps/movements_horseMackerel_initialAge_4_lastAge_16.nc");
+        cmd.put("netcdfmovements.file.map31", dirMaps + "maps/movements_horseMackerel_initialAge_4_lastAge_16.nc");
         cmd.put("netcdfmovements.species.map31", "horseMackerel");
         cmd.put("netcdfmovements.variable.map31", "movements");
         cmd.put("netcdfmovements.nsteps.year.map31", "24");
