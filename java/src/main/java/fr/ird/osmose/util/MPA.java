7eaf9c0d98b4cb21914006c00e00412d58baf63e
barriern <nicolas.barrier@ird.fr>
2022-06-08 08:23:09 +0200

    adding MPA percentage variable

--- java/src/main/java/fr/ird/osmose/util/MPA.java
+++ java/src/main/java/fr/ird/osmose/util/MPA.java
@@ -1,10 +1,10 @@
-/* 
- * 
+/*
+ *
  * OSMOSE (Object-oriented Simulator of Marine Ecosystems)
  * http://www.osmose-model.org
- * 
+ *
  * Copyright (C) IRD (Institut de Recherche pour le Développement) 2009-2020
- * 
+ *
  * Osmose is a computer program whose purpose is to simulate fish
  * populations and their interactions with their biotic and abiotic environment.
  * OSMOSE is a spatial, multispecies and individual-based model which assumes
@@ -15,7 +15,7 @@
  * processes of fish life cycle (growth, explicit predation, additional and
  * starvation mortalities, reproduction and migration) and fishing mortalities
  * (Shin and Cury 2001, 2004).
- * 
+ *
  * Contributor(s):
  * Yunne SHIN (yunne.shin@ird.fr),
  * Morgane TRAVERS (morgane.travers@ifremer.fr)
@@ -23,20 +23,20 @@
  * Philippe VERLEY (philippe.verley@ird.fr)
  * Laure VELEZ (laure.velez@ird.fr)
  * Nicolas Barrier (nicolas.barrier@ird.fr)
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation (version 3 of the License). Full description
  * is provided on the LICENSE file.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- * 
+ *
  */
 
 package fr.ird.osmose.util;
@@ -75,6 +75,11 @@ public class MPA extends OsmoseLinker {
      */
     private int start, end;
 
+    /** Surface of the cell occupied by an MPA.
+     *  Dimension = [ny, nx]
+     */
+    private float[][] percentageMPA;
+
     /**
      * Creates a new MPA.
      *
@@ -98,6 +103,9 @@ public class MPA extends OsmoseLinker {
         } else {
             start = Integer.MAX_VALUE;
         }
+
+        percentageMPA = new float[getGrid().get_ny()][getGrid().get_nx()];
+
     }
 
     /**
@@ -171,6 +179,7 @@ public class MPA extends OsmoseLinker {
                     float val = Float.valueOf(line[i]);
                     if (val > 0.f) {
                         cells.add(getGrid().getCell(i, j));
+                        percentageMPA[j][i] = val;
                     }
                 }
             }
@@ -180,4 +189,27 @@ public class MPA extends OsmoseLinker {
             error("Error loading MPA " + csvFile, ex);
         }
     }
+
+    /**
+     * Returns the percentage of the cell occupied by a MPA.
+     *
+     * @param cell
+     *            Cell value
+     * @return Percentage of MPA
+     */
+    public float getPercentageMPA(Cell cell) {
+        return this.percentageMPA[cell.get_jgrid()][cell.get_igrid()];
+    }
+
+    /**
+     * Returns the percentage of the cell occupied by a MPA
+     *
+     * @param cell
+     *            Cell value
+     * @return Percentage of MPA
+     */
+    public float getPercentageMPA(int i, int j) {
+        return this.percentageMPA[j][i];
+    }
+
 }
