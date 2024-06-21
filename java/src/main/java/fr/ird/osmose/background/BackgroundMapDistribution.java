b2a15bbebb2e50d3fdfd062cad270a8759185b3d
Nicolas Barrier <nicolas.barrier@ird.fr>
2024-06-21 13:57:07 +0200

    Update parameters

--- java/src/main/java/fr/ird/osmose/background/BackgroundMapDistribution.java
+++ java/src/main/java/fr/ird/osmose/background/BackgroundMapDistribution.java
@@ -82,14 +82,14 @@ public class BackgroundMapDistribution extends OsmoseLinker {
     public void init() {
 
         boolean fixedSeed = false;
-        if (!getConfiguration().isNull("movement.randomseed.fixed")) {
-            fixedSeed = getConfiguration().getBoolean("movement.randomseed.fixed");
+        if (!getConfiguration().isNull("simulation.fixedseed.enabled")) {
+            fixedSeed = getConfiguration().getBoolean("simulation.fixedseed.enabled");
         }
         if (fixedSeed) {
             rd1 = new Random(13L ^ iSpecies);
             rd2 = new Random(5L ^ iSpecies);
             rd3 = new Random(1982L ^ iSpecies);
-            warning("Parameter 'movement.randomseed.fixed' is set to true. It means that two simulations with strictly identical initial school distribution will lead to same movement.");
+            warning("Parameter 'simulation.fixedseed.enabled' is set to true. It means that two simulations with strictly identical initial school distribution will lead to same movement.");
         } else {
             rd1 = new Random();
             rd2 = new Random();
